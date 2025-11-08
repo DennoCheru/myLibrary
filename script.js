e5213bc8b3c2d529d37c72884d3a10cfc33154e0
cheru <30118310+DennoCheru@users.noreply.github.com>
2025-11-08 19:04:51 +0300

    Add Book constructor

---
+++ script.js
@@ -0,0 +1,11 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+ if (!new.target) {
+    throw Error('You must use the new operator to call this constructor')-
+ }
+ this.title = title;
+ this.author = author;
+ this.pages = pages;
+ this.read = read;
+}
