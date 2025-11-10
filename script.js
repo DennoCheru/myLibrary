ad2975e5e9645246b0968763100bd12e641f4bd1
cheru <30118310+DennoCheru@users.noreply.github.com>
2025-11-10 14:31:30 +0300

    Add Display books function

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const myLibrary = [];
 
 function Book(title, author, pages, read) {
  if (!new.target) {
-    throw Error('You must use the new operator to call this constructor')-
+    throw Error('You must use the new operator to call this constructor');
  }
  this.id = crypto.randomUUID();
  this.title = title;
@@ -12,5 +12,34 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary() {
-   
+
+}
+
+
+function displayBooks(myLibrary) {
+   const container = document.querySelector('.container');
+   container.textContent = '';
+
+   myLibrary.array.forEach(book => {
+      const bookCard = document.createElement('div');
+      bookCard.classList.add('card');
+
+      const titleEl = document.createElement('h3');
+      titleEl.textContent = `Title: ${book.title}`;
+      bookCard.appendChild(titleEl)
+
+      const authorEl = document.createElement('p');
+      authorEl.textContent = `Author: ${book.author}`;
+      bookCard.appendChild(authorEl);
+
+      const pagesEl = document.createElement('p');
+      pagesEl.textContent =  `Pages: ${book.pages}`;
+      bookCard.appendChild(pagesEl);
+
+      const readEl = document.createElement('p');
+      readEl.textContent = `Read: ${book.read ? "Read" : "Not read"}`;
+      bookCard.appendChild(readEl);
+   });
+   container.appendChild(bookCard);
+      
 }
