2acb130b0a120b7c327565e3c387a08dd4067f1e
cheru <30118310+DennoCheru@users.noreply.github.com>
2025-11-10 11:06:29 +0300

    Create Add book to libary function

--- script.js
+++ script.js
@@ -4,8 +4,13 @@ function Book(title, author, pages, read) {
  if (!new.target) {
     throw Error('You must use the new operator to call this constructor')-
  }
+ this.id = crypto.randomUUID();
  this.title = title;
  this.author = author;
  this.pages = pages;
  this.read = read;
+}
+
+function addBookToLibrary() {
+   
 }
